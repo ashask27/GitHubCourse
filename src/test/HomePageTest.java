7d070121ef491b951e780705d789c92fdc33c8c6
Asha KOLI <ashask27@gmail.com>
2022-01-19 13:03:30 +0000

    my first commit

---
+++ src/test/HomePageTest.java
@@ -0,0 +1,8 @@
+package test;
+
+public class HomePageTest {
+
+	public static void test2() {
+		System.out.println("test2 - Home Page");
+	}
+}
---
+++ src/test/LoginPageTest.java
@@ -0,0 +1,8 @@
+package test;
+
+public class LoginPageTest {
+
+	public static void test1() {
+		System.out.println("test1 - Login Page");
+	}
+}

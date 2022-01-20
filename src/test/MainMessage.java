e8ee0252682e09ec11b74dae5fcbb3ee69258a31
Asha KOLI <ashask27@gmail.com>
2022-01-20 07:22:19 +0000

    feaure 2 done

--- src/test/MainMessage.java
+++ src/test/MainMessage.java
@@ -5,6 +5,8 @@ public class MainMessage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("feature 1");
+		
+		System.out.println("feature 2");
 	}
 
 }
